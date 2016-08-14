d413c618ebbad24d76bbb6e163cde0fad9a824c1
Alex Gerdom <alexgerdom@gmail.com>
2016-08-14 01:38:51 -0400

    fixed bug where nrows and ncols were switched, added gui

--- segment.py
+++ segment.py
@@ -6,6 +6,7 @@ from operator import itemgetter
 from seggui import ControlPanel, filedialog
 import argparse
 import sys
+
 if len(sys.argv) > 1:
     parser = argparse.ArgumentParser()
     parser.add_argument("input_image_path", type=str, help="Path to the image to be chopped up")
@@ -13,8 +14,10 @@ if len(sys.argv) > 1:
     parser.add_argument("height",type=int,default=512,help="Height of full-size image segments")
     parser.add_argument('-d','--dropexcess', action='store_true',
                         help="If -d switch is used, non-full-sized images will be dropped from the output")
-    parser.add_argument('-o', "--output", nargs='?', help='Path to folder to output the image segments to')
-    script_args = parser.parse_args()
+    parser.add_argument('-o', "--output", help='Path to folder to output the image segments to')
+    script_args = parser.parse_args().__dict__
+    print("scriptargs",script_args)
+
 else:
     from functools import partial
     panel = ControlPanel()
@@ -24,8 +27,8 @@ else:
                                           title= "Folder to save the image segments to"))
     panel.add_entry('width','width')
     panel.add_entry('height','height')
-    panel.add_folder_chooser('-o','Output Folder', op=filedialog.askdirectory)
-    panel.add_checkbox('-d','Drop Small Segments',default=0)
+    panel.add_folder_chooser('output','Output Folder', op=filedialog.askdirectory)
+    panel.add_checkbox('dropexcess','Drop Small Segments',default=0)
     script_args = panel.mainloop()
     for key in ('width','height'):
         script_args[key] = int(script_args[key])
@@ -46,10 +49,18 @@ def decompose(img, main_width=512, main_height=512):
     """
     segments = {}  # cropped images with keys (row,column)
     owidth, oheight = img.size
-    nrows = owidth // main_width
-    ncols = oheight // main_height
-    right_excess = owidth - (ncols * main_width)
+
+    nrows = oheight // main_height
     bottom_excess = oheight - (nrows * main_height)
+    ncols = owidth // main_width
+    right_excess = owidth - (ncols * main_width)
+    assert (bottom_excess >= 0)
+    assert (right_excess >= 0)
+    print(ncols,'*',main_width)
+    print('mw',main_width,'mh',main_height)
+    print('o',owidth,oheight)
+    print('nr',nrows,'nc',ncols)
+    print(right_excess,bottom_excess)
     for r_i in range(nrows):
         for c_i in range(ncols):
             left = c_i * main_width
@@ -57,12 +68,12 @@ def decompose(img, main_width=512, main_height=512):
             upper = (r_i * main_height)
             lower = (r_i + 1) * main_height
             segments[(r_i, c_i)] = img.crop((left, upper, right, lower))
-        if right_excess != 0 and not script_args["-d"]:
+        if right_excess != 0 and not script_args["dropexcess"]:
             c_i += 1
             left += main_width
             right = owidth
             segments[(r_i, c_i)] = img.crop((left, upper, right, lower))
-    if bottom_excess != 0 and not script_args["-d"]:
+    if bottom_excess != 0 and not script_args["dropexcess"]:
         r_i += 1
         for c_i in range(ncols):
             left = c_i * main_width
@@ -75,6 +86,14 @@ def decompose(img, main_width=512, main_height=512):
             left += main_width
             right = owidth
             segments[(r_i, c_i)] = img.crop((left, upper, right, lower))
+    # replace this later for efficiency
+    topop = []
+    for k,s in segments.items():
+        if s.width == 0 or s.height==0:
+            topop.append(k)
+    for k in topop:
+        print('popping:',k)
+        segments.pop(k)
     return segments
 
 def show_segments(segment_dict):
@@ -105,6 +124,6 @@ if __name__ == '__main__':
     segs = decompose(img,
                      script_args['width'],
                      script_args['height'])
-    save_segments(segs, script_args['-o'])
+    save_segments(segs, script_args['output'])
 
 
